36edcce6506110d346d7aa8b7bd574e54e1cdf84
aifly <aifly_xc@163.com>
2015-06-22 00:31:55 +0800

    add angularjs

---
+++ js/index.js
@@ -0,0 +1,24 @@
+﻿var model = angular.module("myApp", ["ngRoute"]);
+
+model.controller("myCtrl", ["$scope", "$location", function ($scope, $location) {
+
+    $scope.$location = $location;
+}]);
+angular.element(document).ready(function () {
+    //angular.bootstrap(document, ["myApp"]);
+});
+model.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("/index", {
+        templateUrl: "templates/index.html"
+    }).when("/news", {
+        templateUrl: "templates/news.html"
+    }).when("/html5", {
+        templateUrl: "templates/html5.html"
+    }).when("/css3", {
+        templateUrl: "templates/css3.html"
+    }).when("/js", {
+        templateUrl: "templates/js.html"
+    }).otherwise({
+        redirectTo: "/index"
+    });
+}]);
